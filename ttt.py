513c0f6b45c1842f1ec6cf88fcfc5e69e51875dd
Kurt Neufeld <kneufeld@burgundywall.com>
2016-10-23 15:32:07 -0600

    adding stub tty.py

---
+++ ttt.py
@@ -0,0 +1,11 @@
+"""
+TicTacToe
+
+x goes first
+board is a list of 9 elements from ['x','o','.']. '.' means empty spot.
+"""
+
+states = None # you decide what this should be
+
+def game_state(board):
+    return states # you decide what should get returned here
