22089949f58806f03f88d1699062e00bd2ee4ad6
meejah <meejah@meejah.ca>
2016-10-25 18:43:07 -0600

    more tests, progress -- time was up

--- test_ttt.py
+++ test_ttt.py
@@ -4,10 +4,33 @@ import ttt
 
 class TestTTT(unittest.TestCase):
 
-    def test_1(self):
+    def test_invalid_wrong_size(self):
+        board = [
+            'x', '.', 'o',
+        ]
+        self.assertEqual('invalid', ttt.game_state(board))
+
+    def test_invalid_illegal_char(self):
+        board = [
+            'x', '.', 'o',
+            'x', '.', 'o',
+            'x', '.', 'O',
+        ]
+        self.assertEqual('invalid', ttt.game_state(board))
+
+    def test_x_count(self):
         board = [
             'x', '.', 'o',
             'x', '.', 'o',
             'x', '.', '.',
+        ]
+        self.assertEqual(3, ttt.x_count(board))
+        self.assertEqual(2, ttt.o_count(board))
+
+    def test_x_win(self):
+        board = [
+            'x', 'x', 'x',
+            'o', '.', 'o',
+            '.', '.', '.',
         ]
         self.assertEqual('x wins', ttt.game_state(board))
--- ttt.py
+++ ttt.py
@@ -13,10 +13,23 @@ states = [
     'incomplete',
 ]
 
+def x_count(board):
+    return len([ch for ch in board if ch == 'x'])
+
+def o_count(board):
+    return len([ch for ch in board if ch == 'o'])
+
 def game_state(board):
     if len(board) != 9:
         return 'invalid'
     for c in board:
         if c not in ('x', 'o', '.'):
             return 'invalid'
-    return 'fixme'
+    diff = x_count(board) - y_count(board)
+    if diff not in (0, 1):
+        return 'invalid'
+    # x wins cases (8 cases)
+    xt = ['x', 'x', 'x']
+    if board[:3] == xt or board[3:6] == xt or board[6:] == xt:
+        return 'x wins'
+    
