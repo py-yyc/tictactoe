69e3f26cf9510579218d1dd3975d3c2f5ea4073c
Kurt Neufeld <kneufeld@burgundywall.com>
2016-10-25 18:40:39 -0600

    adding GameStates object

--- ttt.py
+++ ttt.py
@@ -5,7 +5,18 @@ x goes first
 board is a list of 9 elements from ['x','o','.']. '.' means empty spot.
 """
 
-states = None # you decide what this should be
+import types
+
+class GameStates(object):
+    """
+    there are lots of ways to accomplish an enum like object
+    this is the one we're going with since it's simple
+    """
+    invalid    = 0
+    unfinished = 1
+    x_wins     = 2
+    o_wins     = 3
+    draw       = 4
 
 def game_state(board):
     return states # you decide what should get returned here
