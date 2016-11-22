41583a30b9feecd5f6b93f4936e2ef9359f43960
meejah <meejah@meejah.ca>
2016-11-22 16:18:08 -0700

    skeleton setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='pyyyc_tictactoe',
+    version='0.0.0',
+    description='PyYYC collaborative tic-tac-toe learning project',
+    long_description=open('README.md', 'r').read(),
+    keywords=['python'],
+    install_requires=[
+    ],
+    # "pip install -e .[dev]" will install development requirements
+    extras_require={
+        'dev': [
+            'pytest',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    author='PyYYC',
+    author_email='organizers@pyyyc.org',
+    url='https://www.meetup.com/py-yyc/',
+    license='MIT',
+    packages=[
+    ],
+)
