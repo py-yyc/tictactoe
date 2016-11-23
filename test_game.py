480e25fe01d0549d80a0ac905843f4d6ba736e9b
meejah <meejah@meejah.ca>
2016-11-22 17:29:54 -0700

    more tests

--- test_game.py
+++ test_game.py
@@ -8,8 +8,34 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.game = ttt.Game()
 
+    def test_invalid_row(self):
+        with self.assertRaises(Exception) as ctx:
+            self.game.move('x', -1, 0)
+        self.assertEqual("Invalid location", str(ctx.exception))
+
+    def test_invalid_row_big(self):
+        with self.assertRaises(Exception) as ctx:
+            self.game.move('x', 3, 0)
+        self.assertEqual("Invalid location", str(ctx.exception))
+
+    def test_invalid_col(self):
+        with self.assertRaises(Exception) as ctx:
+            self.game.move('x', 0, -1)
+        self.assertEqual("Invalid location", str(ctx.exception))
+
+    def test_invalid_col_big(self):
+        with self.assertRaises(Exception) as ctx:
+            self.game.move('x', 0, 3)
+        self.assertEqual("Invalid location", str(ctx.exception))
+
     def test_x_moves_twice(self):
         self.game.move('x', 0, 0)
         with self.assertRaises(Exception) as ctx:
             self.game.move('x', 0, 1)
+        self.assertEqual("Invalid move", str(ctx.exception))
+
+    def test_x_moves_twice_same_location(self):
+        self.game.move('x', 0, 0)
+        with self.assertRaises(Exception) as ctx:
+            self.game.move('x', 0, 0)
         self.assertEqual("Invalid move", str(ctx.exception))
